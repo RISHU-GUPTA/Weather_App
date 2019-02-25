f14a9bd4771b91f76e4e46cc912e69977bab6e4b
Rishu Gupta <guptarishu04@gmail.com>
2019-02-25 20:20:32 +0530

    console log to UI

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ import Weather from "./components/Weather"
 
 const API_KEY = "e891d92988f04fb1aa4acb2d6f19a97d";
 class App extends React.Component {
+  state={
+    temperature: undefined,
+    city:undefined,
+    country:undefined,
+    humidity: undefined,
+    description:undefined,
+    error:undefined
+    
+  }
   getWeather= async (e)=>{
     e.preventDefault();
     const city = e.target.elements.city.value;
@@ -12,13 +21,27 @@ class App extends React.Component {
     const api_call= await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}`);
   const data = await api_call.json();
   console.log(data);
+  this.setState({
+    temperature:data.main.temp,
+    city:data.name,
+    country:data.sys.country,
+    humidity:data.main.humidity,
+    description:data.weather[0].description,
+    error:""
+  })
 }
 render(){
   return(
     <div>
     <Titles/>
     <Form getWeather={this.getWeather}/>
-    <Weather/>
+    <Weather temperature={
+      this.state.temperature}
+    city={this.state.city} 
+    country={this.state.country} 
+    humidity={this.state.humidity}
+    description={this.state.description}
+    error={this.state.error}/>
     </div>
   );
 }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,7 +2,13 @@ import React from "react"
 class Weather extends React.Component{
     render(){
         return(
-            <div>Weather Components</div>
+            <div>
+       {this.props.city && this.props.country && <p>  Location:  {this.props.city},{this.props.country}</p>} 
+            
+    {this.props.temperature &&  <p>    Temperature:   {this.props.temperature}</p>}
+     {this.props.humidity && <p>   Humidity:    {this.props.humidity}</p>}
+    {this.props.description &&  <p>   Condition:   {this.props.description}</p>}
+            </div>
         )
     }
 }
